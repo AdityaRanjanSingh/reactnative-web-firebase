de7e3cd97b28cc3cf639a670d89a563a31af7410
Aditya Ranjan Singh <adityaranjansingh@Adityas-MacBook-Pro.local>
2020-05-14 21:44:02 +0100

    Updates

---
+++ precache-manifest.6c36eb83bc9e568664b1b476181f67d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7923eda931de857286df",
+    "url": "/static/js/app.1ebcc80a.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "5800bf35f28ec5811965",
+    "url": "/static/js/2.ea5aa156.chunk.js"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "e7cc551b56255d2b97b42ddb8e38734f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e38e15f85d53e3767ede85a586a6323",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
